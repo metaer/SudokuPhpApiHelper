927255468cb6d0bf97d67f7f94b12fc0f4adc799
pavel <popovp2012@gmail.com>
2014-09-28 11:42:50 +0400

    adding jar

--- entry.php
+++ entry.php
@@ -1,14 +1,17 @@
 <?php
-
 header('Content-Type: text/html; charset=utf-8');
 header("Cache-Control: no-cache");
 header("Pragma: no-cache");
 
+$jarFileName = 'javasudokusolverapi.jar';
+
 $inputString = substr($_SERVER['REQUEST_URI'],1);
 putenv('LANG=ru_RU.UTF-8');
 exec(
-    'cd /home/pavel/projects/JavaSudokuSolverAPI/out/artifacts/JavaSudokuSolverAPI_jar && java -jar JavaSudokuSolverAPI.jar '.$inputString,
+    "java -jar $jarFileName ".$inputString,
     $output
 );
 
-var_dump($output);
+foreach ($output as $row) {
+    echo $row;
+}
